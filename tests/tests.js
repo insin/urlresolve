73b4229875f6782eb7d52c1b93521b8564658903
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-12-17 03:38:25 +0000

    [test] Made QUnit tests independent of your cwd; added `npm test` script

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,6 @@
 var qunit = require('qunit')
+  , path = require('path')
 
-qunit.run({ code: {path: '../urlresolve.js', namespace: 'urlresolve'}
-          , tests: [ './urlresolve.js']
+qunit.run({ code: {path: path.join(__dirname, '../urlresolve.js'), namespace: 'urlresolve'}
+          , tests: [path.join(__dirname, 'urlresolve.js')]
           })
