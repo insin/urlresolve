51be76f4d0cc3d75bc72d1aeb78bcba508273c67
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2012-01-18 04:06:52 +0000

    Moved urllib into the /lib directory and removed the browser wrapper

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,6 @@
 var qunit = require('qunit')
   , path = require('path')
 
-qunit.run({ code: {path: path.join(__dirname, '../urlresolve.js'), namespace: 'urlresolve'}
+qunit.run({ code: {path: path.join(__dirname, '../lib/urlresolve.js'), namespace: 'urlresolve'}
           , tests: [path.join(__dirname, 'urlresolve.js')]
           })
